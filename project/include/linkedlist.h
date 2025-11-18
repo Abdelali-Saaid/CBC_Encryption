1fdc844e3ccc50d55d1ff6eb501b5f4d9b555beb
Abdelali Saaid <165439618+Abdelali-Saaid@users.noreply.github.com>
2025-11-18 13:21:21 +0100

    Update linkedlist.h

--- project/include/linkedlist.h
+++ project/include/linkedlist.h
@@ -1,22 +1,14 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-// Définition de la structure pour un nœud dans une liste chaînée
 struct Node {
-  char data;         // Donnée stockée dans le nœud
-  struct Node *next; // Pointeur vers le prochain nœud dans la liste
+    char data;
+    struct Node *next;
 };
 
-// Signature de la fonction pour créer un nouveau nœud
 struct Node *createNode(char data);
-
-// Signature de la fonction pour insérer un nouveau nœud en tête de liste
-void insertNode(struct Node **head, char data);
-
-// Signature de la fonction pour supprimer un nœud spécifique de la liste
-void deleteNode(struct Node **head, char data);
-
-// Signature de la fonction pour afficher les données de la liste
+void appendNode(struct Node **head, char data);
 void displayList(struct Node *head);
+void freeList(struct Node *head);
 
 #endif
